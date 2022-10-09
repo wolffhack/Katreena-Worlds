de58bef29bec4fb442879a2ece0ce4145bfbe77d
wolffhack <69741055+wolffhack@users.noreply.github.com>
2022-10-09 12:21:09 -0500

    game

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
 import { Flex, Center, Text, Button } from "@chakra-ui/react";
 
+
 export default function Header({user, logout, isLoggingOut}) {
+
+    // const navigate = useNavigate();
+    // const navigateKatreeenaWorlds = () => {
+    //     navigate('www.ar-nft.app')
+    // };
+
+
     return(
         <header>
             <Flex px="10" py="6" justifyContent="space-between" bgGradient="Linear(to-br, teal.400, #6200ee)" color="white">
                 <Center>
                     <Text color="white" fontSize="xl" fontWeight="bold">Katreena</Text>
+                </Center>
+                <Center>
+                <Link href="https://ar-nft.app/">Play Katreena-Worlds</Link>
                 </Center>
                 <Center>
                     <Text>{user.getUsername()}</Text>
