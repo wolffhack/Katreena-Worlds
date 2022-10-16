eca6ac04e901f2965b77094a18797580866884a1
wolffhack <69741055+wolffhack@users.noreply.github.com>
2022-10-16 17:53:50 -0500

    axelar

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import { Flex, Center, Text, Button } from "@chakra-ui/react";
 
+export default function Header({ user, logout, isLoggingOut }) {
+  // const navigate = useNavigate();
+  // const navigateKatreeenaWorlds = () => {
+  //     navigate('www.ar-nft.app')
+  // };
 
-export default function Header({user, logout, isLoggingOut}) {
-
-    // const navigate = useNavigate();
-    // const navigateKatreeenaWorlds = () => {
-    //     navigate('www.ar-nft.app')
-    // };
-
-
-    return(
-        <header>
-            <Flex px="10" py="6" justifyContent="space-between" bgGradient="Linear(to-br, teal.400, #6200ee)" color="white">
-                <Center>
-                    <Text color="white" fontSize="xl" fontWeight="bold">Katreena</Text>
-                </Center>
-                <Center>
-                <Link href="https://ar-nft.app/">Play Katreena-Worlds</Link>
-                </Center>
-                <Center>
-                    <Text>{user.getUsername()}</Text>
-                    <Button ml="4" bg="#05cd3e" onClick={logout} disabled={isLoggingOut}>Logout</Button>
-                </Center>
-            </Flex>
-        </header>
-    )
+  return (
+    <header>
+      <Flex
+        px="10"
+        py="6"
+        justifyContent="space-between"
+        bgGradient="Linear(to-br, teal.400, #6200ee)"
+        color="white"
+      >
+        <Center>
+          <Text color="white" fontSize="xl" fontWeight="bold">
+            Katreena
+          </Text>
+        </Center>
+        <Center>
+          <Link href="https://ar-nft.app/" isExternal>
+            <Button>Play Katreena-Worlds</Button>
+          </Link>
+        </Center>
+        <Center>
+          <Text>{user.getUsername()}</Text>
+          <Button ml="4" bg="#05cd3e" onClick={logout} disabled={isLoggingOut}>
+            Logout
+          </Button>
+        </Center>
+      </Flex>
+    </header>
+  );
 }
