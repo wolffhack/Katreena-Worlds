9e00a31c67c5c48b6012e8bcf261617645781bb3
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2022-11-05 22:39:15 -0600

    newchanges

--- components/AuthPage.js
+++ components/AuthPage.js
@@ -1,22 +1,38 @@
+<<<<<<< Updated upstream
 import { Flex, Button, Text, Box} from "@chakra-ui/react";
+=======
+import { Flex, Button, Text, Box, Link, Spacer, Container, Heading,  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,} from "@chakra-ui/react";
+>>>>>>> Stashed changes
 import Head from "next/head";
 import React from "react";
 import Nav from "./menuLanding"
-// import '@google/model-viewer';
+//  import ModelView from '@google/model-viewer';
+import Script from 'next/script'
+import ModelGLB from "../public/faviconKN.png"
+
+
 
 
 export default function AuthPage({metamaskAuth}) {
     return (
     <>
+    <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js" />
       <Head>
       <title>Login / K3na/ documentation
 
       </title>
       </Head>
+<<<<<<< Updated upstream
       <Nav />
       <div className="navLanding">
         <a href="www.google.com">LINK GOOGLE</a>
       </div>
+=======
+    
+>>>>>>> Stashed changes
       <Flex 
       direction="column" 
       justifyContent="center" 
@@ -24,19 +40,46 @@ export default function AuthPage({metamaskAuth}) {
       width="100vw"
       height="100vh"
       bgColor="black"
+      color="white"
       >
+<<<<<<< Updated upstream
+=======
+       
+>>>>>>> Stashed changes
           <Text 
               fontSize="5xl" 
               fontWeight="bold"
               color="white"
-          >Katreena Lands</Text>
+          >Katreena</Text>
+        <Breadcrumb separator=' ' marginLeft="50px"  align-self= "flex-end">
+                <BreadcrumbItem isCurrentPage>
+                  <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem>
+                  <BreadcrumbLink href='#'>Metaverse</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem >
+                  <BreadcrumbLink href='#'>White Paper</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem >
+                  <BreadcrumbLink href='#'>Marketplace</BreadcrumbLink>
+                </BreadcrumbItem>
+        </Breadcrumb>
+        
+          
           <Button 
               color="white" 
               bgColor="#05cd3e"
               size="lg"
               mt="6"
               onClick={metamaskAuth}
-          >Login with Metamask</Button>
+              marginBottom = "25px"
+              marginRight="20px"
+              maxWidth="100px"
+          >Login</Button>
       </Flex>  
       <Box>
       {/* <model-viewer
@@ -49,6 +92,28 @@ export default function AuthPage({metamaskAuth}) {
         auto-rotate
     ar
   ></model-viewer>     */}
+<<<<<<< Updated upstream
     </Box>           
+=======
+        </Container >
+        <model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="katree.glb" ar environment-image="shared-assets/environments/moon_1k.hdr" poster="3Dmarket.png" shadow-intensity="1" camera-controls touch-action="pan-y"></model-viewer>
+        <Flex h="100%" align="center" justify="center" direction="column" >
+          <Container >
+        <Heading>Discover the unique digital art of 3D AR NFT's</Heading>
+        <Text>Digital marketplace for crypto collectibles and Ecological backend NFT's, Buy, sell, discover exclusive assets and save the planet.</Text>
+        <Flex>
+        <Button>Explore</Button>
+        <Button>Create</Button>
+       
+        </Flex>
+        </Container>
+        
+        </Flex>
+        
+     
+    </Flex> 
+    <Flex h="10vh" bgColor="red" color="white">
+      Footer</Flex>          
+>>>>>>> Stashed changes
   </>
   )}
